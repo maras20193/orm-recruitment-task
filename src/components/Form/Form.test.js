b44440083edce5702bdf68c38dd365e5dbeacf4d
Łukasz Markowski <lucas20193@gmail.com>
2021-10-02 13:40:34 +0200

    add Form component tests

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,12 +1,48 @@
+import { createRef } from 'react';
 import { render, screen } from '@testing-library/react';
-import Form from '../Form/Form';
+import { BrowserRouter as Router, } from 'react-router-dom';
 
+import Form from './Form';
+
+const testData = {
+  name: "Header",
+  inputs: [
+    {
+    label: "Input",
+    name: "email",
+    type: "text"
+    }],
+  message1: "message one",
+  message2: "message two",
+  link: "/link"
+}
+
+const emailRef = createRef(null);
+const refs = {email: emailRef};
+
+const errors = {
+  email: '',
+}
 
 describe("Form test", () => {
 
-  test('form problem', async () => {
-    render(<Header name={"My header test"}/>);
-    const headerElement = screen.getByRole("heading");
-    expect(headerElement).toHaveTextContent("My header test");
+  test('Form render properly with given props', async () => {
+    render(
+      <Router>
+        <Form formData={testData} refs={refs} errors={errors}/>
+      </Router>);
+
+    const headerElement = screen.getByText(/header/i);
+    expect(headerElement).toBeInTheDocument();
+
+    const messageOneElement = screen.getByText(/message one/i);
+    expect(messageOneElement).toBeInTheDocument();
+
+    const messageTwoElement = screen.getByText(/message two/i);
+    expect(messageTwoElement).toBeInTheDocument();
+
+    const inputElement = screen.getByRole('textbox', /email/i);
+    expect(inputElement).toBeInTheDocument();
+
   });
 })
